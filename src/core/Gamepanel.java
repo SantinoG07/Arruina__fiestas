d2910bc662e5fe93fc56fb126901a6e7c0b556ec
SantinoG07 <santino.gennuso.m@gmail.com>
2025-09-29 22:04:02 -0300

    :alembic: Intento de introduccion perspectiva isometrica (No funciona bien)

--- src/core/Gamepanel.java
+++ src/core/Gamepanel.java
@@ -4,6 +4,7 @@ import javax.swing.JPanel;
 
 import entities.Player;
 import input.Inputhandler;
+import world.Maps;
 
 import java.awt.Graphics;
 import java.awt.Color;
@@ -11,6 +12,7 @@ import java.awt.Color;
 public class Gamepanel extends JPanel {
 	
 	private Player player;
+    private Maps map;
 	private Inputhandler input;
 	
 	public Gamepanel() {
@@ -19,20 +21,25 @@ public class Gamepanel extends JPanel {
 		this.setFocusable(true);
 		this.requestFocusInWindow();
 		
-		
-		player = new Player(100, 100, input);
+
+        map = new Maps(10, 10);
+        player = new Player(5, 5, input);
 	}
 	
 
     public void update() {
-        player.actualizar(); // Actualizamos jugador
+        if (input.s) player.movimiento(-1, 0);   // arriba-izquierda
+        if (input.d) player.movimiento(0, 1);    // arriba-derecha
+        if (input.w) player.movimiento(1, 0);    // abajo-derecha
+        if (input.a) player.movimiento(0, -1);  
     }
     @Override	//Indica que esta sobreescribiendo un metodo de la libreria
     protected void paintComponent(Graphics g) { //El protected indica que solo se puede usar en clases del mismo paquete
         super.paintComponent(g); //PREGUNTAR
         
-        
-        player.dibujar(g);
+
+        map.dibujarM(g);
+        player.dibujarJ(g);
     }
     
 }
--- src/entities/Player.java
+++ src/entities/Player.java
@@ -4,22 +4,26 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 import input.Inputhandler;
+import world.Isoutils;
 
 public class Player {
-	public double posX, posY, height, width, speed;
+	//public double posX, posY, height, width, speed;
+	public int row, col;
 	
 	private Inputhandler input;
 	
 	public Player(int comienzox, int comienzoy, Inputhandler input) {
-		this.posX = comienzox;
+		/*this.posX = comienzox;
 		this.posY = comienzoy;
 		this.width = 400;
 		this.height = 40;
 		this.speed = 5;
-		this.input = input;
+		this.input = input;*/
+        this.row = row;
+        this.col = col;
 	}
 	
-	public void actualizar() {
+	/*public void actualizar() {
 		if(input.w) posY -=speed;
 		if(input.s) posY +=speed;
 		if(input.a) posX -=speed;
@@ -28,10 +32,22 @@ public class Player {
 	
 	
 	
-	public void dibujar(Graphics g) {
+	public void dibujarJ(Graphics g) {
         // Ejemplo: dibujar jugador
         g.setColor(Color.GREEN);
         g.fillRect((int)posX, (int)posY, (int)width, (int)height);
+	}*/
+	
+	public void movimiento(int dr, int dc) {
+		row+=dr;
+		col+=dc;
+	}
+	
+	public void dibujarJ(Graphics g) {
+		int x = Isoutils.ToscreenX(row, col) + 300;
+		int y = Isoutils.ToscreenX(row, col) + 50;
+        g.setColor(Color.GREEN);
+        g.fillOval(x - 10, y - 20, 20, 20); 
 	}
 	
 	
